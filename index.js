e6d357d803e05538377bfd69c5a64d970ec2ddfa
tojonir <tojonirinaa355@gmail.com>
2023-02-09 20:42:34 +0300

    add mail me endpoint

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 const express = require("express");
 const { json } = require("express");
+const cors = require("cors");
+const yup = require("yup");
+
+const mailMeSchema = yup.object({
+  name: yup.string().required(),
+  mail: yup.string().required(),
+  subject: yup.string().required(),
+  message: yup.string().required(),
+});
 
 const app = express();
 app.use(json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello from resume!");
@@ -26,7 +36,7 @@ app.post("/", async (req, res) => {
 
   await transporter.sendMail(
     {
-      from: "tojonirinandrianarijaona@gmail.com",
+      from: process.env.EMAIL_USER,
       to: req.body.to,
       subject: "ANDRIANARIJAONA Tojonirina",
       html: {
@@ -34,6 +44,9 @@ app.post("/", async (req, res) => {
       },
       encoding: "text/html",
       attachments: [
+        {
+          path: "./assets/ANDRIANARIJAONA Tojonirina.pdf",
+        },
         {
           path: "./assets/in.png",
           cid: "in",
@@ -47,6 +60,37 @@ app.post("/", async (req, res) => {
           cid: "profile",
         },
       ],
+    },
+    (err) => {
+      if (err) return res.status(400).json(err);
+      return res.status(200).send("sent");
+    }
+  );
+});
+
+app.post("/mail-me", async (req, res) => {
+  try {
+    mailMeSchema.validateSync(req.body);
+  } catch (error) {
+    return res.status(401).json(error.message);
+  }
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_AUTH,
+    },
+  });
+
+  await transporter.sendMail(
+    {
+      from: req.body.mail,
+      to: process.env.EMAIL_USER,
+      subject: req.body.subject,
+      html: `<p>${req.body.name}</p><p>${req.body.message}</p>`,
     },
     (err) => {
       if (err) return res.status(400).json(err);
